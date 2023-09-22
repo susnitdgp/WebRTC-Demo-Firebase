447c607905f8c650771a0ebe77923b2ea94e1bcb
Susanta Goswami <susnitdgp@gmail.com>
2023-09-22 15:19:36 +0000

    Committed At Fri Sep 22 15:19:36 UTC 2023

--- main.js
+++ main.js
@@ -6,3 +6,12 @@ const servers = {
   ],
   iceCandidatePoolSize: 10,
 };
+
+/ HTML elements
+const webcamButton = document.getElementById('webcamButton');
+const webcamVideo = document.getElementById('webcamVideo');
+const callButton = document.getElementById('callButton');
+const callInput = document.getElementById('callInput');
+const answerButton = document.getElementById('answerButton');
+const remoteVideo = document.getElementById('remoteVideo');
+const hangupButton = document.getElementById('hangupButton');
